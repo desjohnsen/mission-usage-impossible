04ef743a71830ba07e6014e559d703c9d1706058
MaximilianHedman <maximilian.hedman@chasacademy.se>
2025-06-11 15:25:10 +0200

    Added dotenv for firebase config

---
+++ firebase/firebase.config.ts
@@ -0,0 +1,30 @@
+import Constants from "expo-constants";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { CustomExpoConfig } from "../app.config";
+
+const {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+} = (Constants.expoConfig as CustomExpoConfig)?.extra || {};
+
+const firebaseConfig = {
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+    measurementId: measurementId,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
+
